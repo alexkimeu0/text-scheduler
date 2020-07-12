2cb7b2e5bc95104c449f98c03cdbe448a39b1ea4
alexkimeu0 <kim3ual3x@gmail.com>
2020-07-12 17:25:15 +0300

    added some messages

--- messages.js
+++ messages.js
@@ -1,7 +1,8 @@
 module.exports = [
-  "What the hell!",
-  "Leo yetu ndo hujenga kesho imara",
-  "The day the world went away.",
-  "Only the paranoid survive",
-  "Utachukua muda!",
+  "Valentine's Day only comes once a year, but every day you fill my life with joy. Thank you for all you do in our relationship!",
+  "No matter where I go, the warmest place will always be in your arms. Sweetheart I love you today, tomorrow, and forever after.",
+  "I love you for all that you are, all that you've been, and all that you're yet to be",
+  "I can't imagine what life would have been like without you",
+  "Falling in love is like looking at the stars. If you pick one out of the billions and stare at it long enough all the others will melt away.",
+  "What on earth did I think about all the time before you?",
 ];
